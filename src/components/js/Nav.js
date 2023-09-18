d33ee9a0e827434308ab9f5359b1b3e252e38b3a
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2023-09-18 20:33:41 +0530

    Hello Bar

--- src/components/js/Nav.js
+++ src/components/js/Nav.js
@@ -3,9 +3,13 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expanded", false)
-    : navToggle.setAttribute("aria-expanded", true);
-  primaryNav.toggleAttribute("data-visible");
-  primaryHeader.toggleAttribute("data-overlay");
+  if (primaryNav.hasAttribute("data-visible")) {
+    navToggle.setAttribute("aria-expanded", false);
+    primaryNav.removeAttribute("data-visible"); // Remove the data-visible attribute to hide the navigation menu
+    primaryHeader.removeAttribute("data-overlay"); // Remove the data-overlay attribute to hide the overlay
+  } else {
+    navToggle.setAttribute("aria-expanded", true);
+    primaryNav.setAttribute("data-visible", ""); // Add the data-visible attribute to show the navigation menu
+    primaryHeader.setAttribute("data-overlay", ""); // Add the data-overlay attribute to show the overlay
+  }
 });
