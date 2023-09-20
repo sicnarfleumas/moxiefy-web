e77db071690824a8e5b539fa74e88e629ab94983
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2023-09-20 12:00:41 +0530

    Nav fixed

--- src/components/js/Nav.js
+++ src/components/js/Nav.js
@@ -2,14 +2,28 @@ const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
+// Function to close the mobile navigation
+function closeMobileNav() {
+  primaryNav.removeAttribute("data-visible");
+  navToggle.setAttribute("aria-expanded", false);
+  primaryHeader.removeAttribute("data-overlay");
+}
+
+// Event listener for the toggle button
 navToggle.addEventListener("click", () => {
   if (primaryNav.hasAttribute("data-visible")) {
-    navToggle.setAttribute("aria-expanded", false);
-    primaryNav.removeAttribute("data-visible"); // Remove the data-visible attribute to hide the navigation menu
-    primaryHeader.removeAttribute("data-overlay"); // Remove the data-overlay attribute to hide the overlay
+    closeMobileNav();
   } else {
     navToggle.setAttribute("aria-expanded", true);
-    primaryNav.setAttribute("data-visible", ""); // Add the data-visible attribute to show the navigation menu
-    primaryHeader.setAttribute("data-overlay", ""); // Add the data-overlay attribute to show the overlay
+    primaryNav.setAttribute("data-visible", "");
+    primaryHeader.setAttribute("data-overlay", "");
   }
+});
+
+// Event listeners for navigation items to close the mobile nav when clicked
+const navItems = primaryNav.querySelectorAll("a"); // Change "a" to the selector for your navigation items
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    closeMobileNav();
+  });
 });
