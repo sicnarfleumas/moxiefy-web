21219feffbab45a1b72d19e34a1debced860bd6e
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2023-09-27 20:34:46 +0530

    meta description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Moxiefy | Content and Design Solutions';
-export const SITE_DESCRIPTION = 'Branding Solutions';
+export const SITE_DESCRIPTION = 'Moxiefy is a creative marketing service for brands wanting to elevate their digital presence. Services include content writing & graphic designing for all platforms';
